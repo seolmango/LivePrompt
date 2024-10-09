93a4af47d501815a86b95163f884f4b38e3d5424
Seolchaehwan <seolchaehwan70@gmail.com>
2024-10-09 14:12:24 +0900

    아니면 설마 이거니?

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from run import socketio, app
 
-application = socketio
+application = app
 
 if __name__ == '__main__':
-    application.run(app, host='0.0.0.0', port=8000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
